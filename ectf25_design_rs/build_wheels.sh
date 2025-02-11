b8f145cad391fe09b813cf99dd4e59eb60e61c7d
Ethan Chapman <ethan.chapman0@outlook.com>
2025-02-11 12:27:55 -0700

    more wheels

--- ectf25_design_rs/build_wheels.sh
+++ ectf25_design_rs/build_wheels.sh
@@ -1,10 +1,10 @@
 source ../venv/bin/activate
 
 # ARM Linux
-docker run --rm -v $(pwd):/io -v $(pwd)/../libectf:/libectf ghcr.io/pyo3/maturin build --release
+docker run --rm -v $(pwd):/io -v $(pwd)/../libectf:/libectf ghcr.io/pyo3/maturin build --release -i python3.11 python3.12 python3.13
 
 # x86_64 Linux
-docker run --platform linux/amd64 --rm -v $(pwd):/io -v $(pwd)/../libectf:/libectf ghcr.io/pyo3/maturin build --release
+docker run --platform linux/amd64 --rm -v $(pwd):/io -v $(pwd)/../libectf:/libectf ghcr.io/pyo3/maturin build --release -i python3.11 python3.12 python3.13
 
 # MacOS
 CARGO_TARGET_DIR=target maturin build --release --target universal2-apple-darwin
