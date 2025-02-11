f3dd0a3396523378792adae4fe02c9026da35e31
Ethan Chapman <ethan.chapman0@outlook.com>
2025-02-11 12:07:45 -0700

    wheels for u

--- ectf25_design_rs/build_wheels.sh
+++ ectf25_design_rs/build_wheels.sh
@@ -11,3 +11,6 @@ CARGO_TARGET_DIR=target maturin build --release --target universal2-apple-darwin
 
 # Windows (not working)
 # CARGO_TARGET_DIR=target maturin build --release -i 3.9 --target x86_64-pc-windows-msvc
+
+rm -rf wheels
+cp -r target/wheels wheels
