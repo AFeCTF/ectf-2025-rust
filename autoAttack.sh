1c74ece483f4a307a2d79dcdf5e919b7332ddf12
jl4wr <jkuhlman20@gmail.com>
2025-02-25 15:07:51 -0700

    removed bash for attacking

--- autoAttack.sh
+++
@@ -1,8 +0,0 @@
-# This script will autopull flags for designs that have little to no security
-flagLengthInBytes=32;
-
-aboveText=$(cat frame_playback.json | grep  -oE ".{0,$flagLengthInBytes}5e20666c6167205e" | head -1 | cut -c1-$flagLengthInBytes | xxd -r -p)
-
-flag="ectf{recording_$aboveText}"
-
-echo $flag
